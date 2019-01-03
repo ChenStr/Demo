3380b6ecf11b66a6a9e4bb146604a695be961f82
Administrator <a>
2019-01-03 11:12:01 +0800

    333

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ header("Access-Control-Allow-Origin : http://10.30.29.56:63341");
 header("Access-Control-Allow-Credentials : true");
 header('Access-Control-Allow-Headers : Content-Type');
 
+//修改一下
 //echo '{"userName":"123"}';
 $data=array("userName"=>"123");
 echo json_encode($data);
